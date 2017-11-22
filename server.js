6424bab2f3b22ff1c0277a95280078c6d4120d75
adam_polczynski@yahoo.com <adam_polczynski@yahoo.com>
2017-11-22 04:07:24 +0100

    changed port

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'app/public/index.html'));
 });
 const port = process.env.PORT || 8080;
 
